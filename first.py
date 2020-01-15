faff5bc6c1508ac2a95f37758ea3e3674f908944
Akshay <akshaymadhira@gmail.com>
2020-01-15 11:16:00 +0530

    first python program I made

---
+++ first.py
@@ -0,0 +1,27 @@
+# print ("hello world")
+# myval = None
+# print(myval
+# )
+
+#age = 12
+'''
+ def silly_age(age):
+    if age >=10 and age <= 13:
+        print("What is ding ding dish")
+    else:
+        print('Hu')
+
+silly_age(age)
+
+'''
+
+'''print ("hello")
+'''
+def moon_weight(weight, delta):
+    new_weight = weight + (delta * 15)
+    return new_weight
+
+
+resp = moon_weight(31, 0.25)
+
+print (resp)
